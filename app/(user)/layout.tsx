8b3813107706a582ecda8ff82b8aab4efe1f0dae
marksocoh <marksoko001@gmail.com>
2025-03-01 05:20:55 +0300

    Added course Grid Section

--- app/(user)/layout.tsx
+++ app/(user)/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import {ClerkProvider} from "@clerk/nextjs";
 import Header from "@/components/Header";
+import { SanityLive } from "@/sanity/lib/live";
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -16,9 +17,9 @@ export default function UserLayout({
     <ClerkProvider>
       <div className="min-h-screen flex flex-col">
           <Header/>
-          <main className="flex-1">{children}</main>
-          
+          <main className="flex-1">{children}</main>    
       </div>
+      <SanityLive/>
     </ClerkProvider>
   );
 }
--- app/(user)/page.tsx
+++ app/(user)/page.tsx
@@ -1,19 +1,19 @@
 import Hero from "@/components/Hero";
-// import { CourseCard } from "@/components/CourseCard";
-// import { getCourses } from "@/sanity/lib/courses/getCourses";
+import { CourseCard } from "@/components/CourseCard";
+import { getCourses } from "@/sanity/lib/courses/getCourses";
 
 // export const dynamic = "force-static";
 // export const revalidate = 3600; // revalidate at most every hour
 
 export default async function Home() {
-  // const courses = await getCourses();
+  const courses = await getCourses();
 
   return (
     <div className="min-h-screen bg-background">
       <Hero />
 
       {/* Courses Grid */}
-      {/* <div className="container mx-auto px-4">
+      <div className="container mx-auto px-4">
         <div className="flex items-center gap-4 py-8">
           <div className="h-px flex-1 bg-gradient-to-r from-border/0 via-border to-border/0" />
           <span className="text-sm font-medium text-muted-foreground">
@@ -31,7 +31,7 @@ export default async function Home() {
             />
           ))}
         </div>
-      </div> */}
+      </div>
     </div>
   );
 }
